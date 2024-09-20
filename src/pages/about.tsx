a5aae4e830db8794e7129224282baeb4d2743aa2
Supratim-Pathak <supratimpathak@gmail.com>
2024-09-20 10:21:23 +0530

    ==

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -80,8 +80,8 @@ export default function WorkExperience() {
             </a>
           </li>
           {/*-------------- Codeclouds IT solutions pvt. ltd end -------------- */}
-          {/*-------------- Devant IT solutions pvt. ltd start -------------- */}
 
+          {/*-------------- Devant IT solutions pvt. ltd start -------------- */}
           <li className="mb-10 ms-4">
             <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700" />
             <h2 className="text-2xl font-semibold text-white">
@@ -137,6 +137,59 @@ export default function WorkExperience() {
             </a>
           </li>
           {/*-------------- Devant IT solutions pvt. ltd end -------------- */}
+
+          {/*-------------- Ejob start -------------- */}
+          <li className="mb-10 ms-4">
+            <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700" />
+            <h2 className="text-2xl font-semibold text-white">
+              <Link href={"https://www.ejobindia.com/"}>Ejobindia</Link>
+            </h2>
+            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500 py-2">
+              Web development training | March 2021 - August 2021
+              {/* <span className="text-white">(Present)</span> */}
+            </time>
+            {/* <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                Marketing UI design in Figma
+            </h3> */}
+            <ul className="list-disc ml-5 text-base font-normal text-gray-500 dark:text-gray-400">
+              <li className="mb-4 mt-3">
+                Completed Professional web development training.
+              </li>
+              <li className="mb-4 mt-3">
+                Gained in-depth Knowledge of various types of technologies like
+                CodeIgniter, HTML, CSS, JS, PHP, HTML, CSS, Bootstrap, Ajax,
+                MVC, Jquery, API.
+              </li>
+              <li className="mb-4 mt-3">
+                Worked on a live project and developed a covid vaccine booking
+                web application and also developed a blog website (cms) using
+                CodeIgniter.
+              </li>
+            </ul>
+
+            <a
+              href="#"
+              className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+            >
+              Learn more{" "}
+              <svg
+                className="w-3 h-3 ms-2 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </a>
+          </li>
+          {/*-------------- Ejob end -------------- */}
         </ol>
       </div>
     </>

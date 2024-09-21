0cae26ff50e0ed792eac1d09ad1f34e27d905b9f
Supratim-Pathak <supratimpathak@gmail.com>
2024-09-22 00:51:44 +0530

    ==

---
+++ src/components/Seo.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "next/head";
+export default function Seo() {
+  return (
+    <div>
+      <Head>
+        <title>My page title</title>
+        <meta property="og:title" content="My page title" key="title" />
+      </Head>
+      <Head>
+        <meta property="og:title" content="My new title" key="title" />
+      </Head>
+      <p>Hello world!</p>
+    </div>
+  );
+}

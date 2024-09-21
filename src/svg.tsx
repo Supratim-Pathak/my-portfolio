c9f7031fed5d3cebde0de7e08fcc91a3e03391f9
Supratim-Pathak <supratimpathak@gmail.com>
2024-09-22 01:32:29 +0530

    ==

--- src/svg.tsx
+++ src/svg.tsx
@@ -3,154 +3,49 @@ import Image from "next/image";
 export const HTML5 = () => {
   return (
     <>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 512 512"
-        fill="#dd4b25"
-        className="flex-shrink-0 h-7 w-7"
-      >
-        <path d="M64 32l34.94 403.21L255.77 480 413 435.15 448 32zm308 132H188l4 51h176l-13.51 151.39L256 394.48l-98.68-28-6.78-77.48h48.26l3.42 39.29L256 343.07l53.42-14.92L315 264H148l-12.59-149.59H376.2z"></path>
-      </svg>
+      <Image src="/Html.png" width={35} height={35} alt="Css" />
     </>
   );
 };
 export const CSS = () => {
   return (
     <>
-      {/* <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="ionicon flex-shrink-0 h-7 w-7"
-        viewBox="0 0 128 128"
-      >
-        <path
-          fill="#1572B6"
-          d="M18.814 114.123l-10.054-112.771h110.48l-10.064 112.754-45.243 12.543-45.119-12.526z"
-        />
-        <path
-          fill="#33A9DC"
-          d="M64.001 117.062l36.559-10.136 8.601-96.354h-45.16v106.49z"
-        />
-        <path
-          fill="#fff"
-          d="M64.001 51.429h18.302l1.264-14.163h-19.566v-13.831h34.681999999999995l-.332 3.711-3.4 38.114h-30.95v-13.831z"
-        />
-        <path
-          fill="#EBEBEB"
-          d="M64.083 87.349l-.061.018-15.403-4.159-.985-11.031h-13.882l1.937 21.717 28.331 7.863.063-.018v-14.39z"
-        />
-        <path
-          fill="#fff"
-          d="M81.127 64.675l-1.666 18.522-15.426 4.164v14.39l28.354-7.858.208-2.337 2.406-26.881h-13.876z"
-        />
-        <path
-          fill="#EBEBEB"
-          d="M64.048 23.435v13.831000000000001h-33.407999999999994l-.277-3.108-.63-7.012-.331-3.711h34.646zM64.001 51.431v13.831000000000001h-15.209l-.277-3.108-.631-7.012-.33-3.711h16.447z"
-        />
-      </svg> */}
-
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        x="0px"
-        y="0px"
-        width="100"
-        height="100"
-        viewBox="0 0 48 48"
-      >
-        <path fill="#0277BD" d="M41,5H7l3,34l14,4l14-4L41,5L41,5z"></path>
-        <path fill="#039BE5" d="M24 8L24 39.9 35.2 36.7 37.7 8z"></path>
-        <path
-          fill="#FFF"
-          d="M33.1 13L24 13 24 17 28.9 17 28.6 21 24 21 24 25 28.4 25 28.1 29.5 24 30.9 24 35.1 31.9 32.5 32.6 21 32.6 21z"
-        ></path>
-        <path
-          fill="#EEE"
-          d="M24,13v4h-8.9l-0.3-4H24z M19.4,21l0.2,4H24v-4H19.4z M19.8,27h-4l0.3,5.5l7.9,2.6v-4.2l-4.1-1.4L19.8,27z"
-        ></path>
-      </svg>
+      <Image src="/css.png" width={35} height={35} alt="Css" />
     </>
   );
 };
 export const JS = () => {
   return (
     <>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 512 512"
-        fill="#EFD81D"
-        className="flex-shrink-0 h-7 w-7"
-      >
-        <path d="M0 0v512h512V0H0zm274.286 397.714c0 49.84-29.44 74.137-72.057 74.137-38.492 0-60.835-19.931-72.172-43.988l39.177-23.714c7.555 13.405 13.292 24.742 29.783 24.742 13.714 0 24.983-6.182 24.983-30.24V237.714h50.286v160zm113.543 72.995c-44.675 0-73.543-20.16-87.635-48L339.429 400c10.285 16.846 23.714 28.069 47.428 28.069 19.932 0 31.509-8.823 31.509-22.572 0-16.491-11.92-22.331-33.92-32l-12.023-5.166c-34.72-14.765-57.737-33.325-57.737-72.514 0-36.08 27.485-62.434 70.445-62.434 30.595 0 52.572 9.508 68.4 37.348L416 294.857c-8.251-14.777-17.143-20.571-30.926-20.571-14.091 0-23.028 8.937-23.028 20.571 0 14.434 8.937 20.274 29.554 29.212l12.023 5.154c40.903 17.531 63.931 35.428 63.931 75.611.012 43.315-34.011 65.875-79.725 65.875z"></path>
-      </svg>
+      <Image src="/Js.png" width={30} height={30} alt="Picture of the author" />
     </>
   );
 };
 export const NODE = () => {
   return (
     <>
-      {/* <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="#6FA660"
-        viewBox="0 0 512 512"
-        className="flex-shrink-0 h-7 w-7"
-      >
-        <path d="M429.76 130.07L274.33 36.85a37 37 0 00-36.65 0L82.24 130.06A38.2 38.2 0 0064 162.83V349a38.26 38.26 0 0018.24 32.8L123 406.14l.23.13c20.58 10.53 28.46 10.53 37.59 10.53 32.14 0 52.11-20.8 52.11-54.29V182a8.51 8.51 0 00-8.42-8.58h-22.38a8.51 8.51 0 00-8.42 8.58v180.51a15 15 0 01-6.85 13.07c-5.9 3.6-14.47 2.84-24.14-2.15l-39.06-23.51a1.1 1.1 0 01-.48-.92V165.46a1.32 1.32 0 01.59-1.06l151.84-93a.82.82 0 01.73 0l151.93 93a1.34 1.34 0 01.55 1.1V349a1.28 1.28 0 01-.45 1l-152.06 90.65a1.22 1.22 0 01-.8 0l-38.83-23.06a7.8 7.8 0 00-7.83-.41l-.34.2c-10.72 6.35-13.6 8-23.54 11.62-1.62.59-5.43 2-5.76 5.77s3.29 6.45 6.51 8.32l51.9 31.87a35.67 35.67 0 0018.3 5.07h.58a35.87 35.87 0 0017.83-5.07l155.43-93.13A38.37 38.37 0 00448 349V162.83a38.21 38.21 0 00-18.24-32.76z"></path>{" "}
-        <path d="M307.88 318.05c-37.29 0-45.24-10.42-47.6-27.24a8.43 8.43 0 00-8.22-7.32h-19.8a8.44 8.44 0 00-8.26 8.58c0 14.58 5.12 62.17 83.92 62.17 24.38 0 44.66-5.7 58.63-16.49S388 311.26 388 292.55c0-37.55-24.5-47.83-72.75-54.55-49.05-6.82-49.05-10.29-49.05-17.89 0-5.47 0-18.28 35.46-18.28 25.23 0 38.74 3.19 43.06 20a8.35 8.35 0 008.06 6.67h19.87a8.24 8.24 0 006.16-2.86 8.91 8.91 0 002.12-6.44c-2.57-35.55-28.56-53.58-79.24-53.58-46.06 0-73.55 20.75-73.55 55.5 0 38.1 28.49 48.87 71.29 53.33 50 5.17 50 12.71 50 19.37.03 10.38-4.28 24.23-41.55 24.23z"></path>
-      </svg> */}
-      <Image
-        src="/Node.png"
-        width={30}
-        height={30}
-        alt="Picture of the author"
-      />
+      <Image src="/Node.png" width={30} height={30} alt="Node" />
     </>
   );
 };
 export const REACT = () => {
   return (
     <>
-      <img className="flex-shrink-0 h-7 w-7" src="../React.png" />
+      <Image src="/React.png" width={35} height={35} alt="React" />
     </>
   );
 };
 export const NEXT = () => {
   return (
     <>
-      <img
-        src="https://svgur.com/i/_tx.svg"
-        className="flex-shring-0 h-7 w-7"
-      />
+      <Image src="/Next.png" width={35} height={35} alt="Next" />
     </>
   );
 };
 export const TAILWIND = () => {
   return (
     <>
-      <svg
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        className="flex-shrink-0 h-7 w-7"
-      >
-        <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M6.333 9.933C7.088 6.911 8.978 5.4 12 5.4c4.533 0 5.1 3.4 7.367 3.967 1.511.377 2.833-.189 3.966-1.7-.755 3.022-2.644 4.533-5.666 4.533-4.534 0-5.1-3.4-7.367-3.967-1.511-.378-2.833.189-3.967 1.7zm-5.666 6.8C1.422 13.711 3.31 12.2 6.333 12.2c4.534 0 5.1 3.4 7.367 3.967 1.51.377 2.833-.19 3.967-1.7C16.91 17.489 15.022 19 12 19c-4.533 0-5.1-3.4-7.367-3.967-1.511-.378-2.833.189-3.966 1.7z"
-          fill="url(#prefix__paint0_linear)"
-        ></path>{" "}
-        <defs>
-          <linearGradient
-            id="prefix__paint0_linear"
-            x1=".667"
-            y1="-6.689"
-            x2="23.333"
-            y2="31.089"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stop-color="#2383AE"></stop>{" "}
-            <stop offset="1" stop-color="#6DD7B9"></stop>
-          </linearGradient>
-        </defs>
-      </svg>
+      <Image src="/tailwind.png" width={35} height={35} alt="React" />
     </>
   );
 };
@@ -184,42 +79,42 @@ export const MONGO = () => {
 export const EXPRESS = () => {
   return (
     <>
-      <img className="flex-shrink-0 h-7 w-7" src="../Express.png" />
+      <Image src="/Express.png" width={35} height={35} alt="React" />
     </>
   );
 };
 export const AWS = () => {
   return (
     <>
-      <img className="flex-shrink-0 h-7 w-7" src="../AWS.png" />
+      <Image src="/AWS.png" width={35} height={35} alt="React" />
     </>
   );
 };
 export const DOCKER = () => {
   return (
     <>
-      <img className="flex-shrink-0 h-7 w-7" src="../DOCKER.png" />
+      <Image src="/DOCKER.png" width={35} height={35} alt="React" />
     </>
   );
 };
 export const TYPESCRIPT = () => {
   return (
     <>
-      <img className="flex-shrink-0 h-7 w-7" src="../TS.png" />
+      <Image src="/TS.png" width={35} height={35} alt="React" />
     </>
   );
 };
 export const GIT = () => {
   return (
     <>
-      <img className="flex-shrink-0 h-7 w-7" src="../GIT.png" />
+      <Image src="/GIT.png" width={35} height={35} alt="React" />
     </>
   );
 };
 export const LARAVEL = () => {
   return (
     <>
-      <img className="flex-shrink-0 h-7 w-7" src="../laravel.png" />
+      <Image src="/laravel.png" width={35} height={35} alt="React" />
     </>
   );
 };
